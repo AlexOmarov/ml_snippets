3f14343cc7b88bb21cd9e8b09eaa2e1ca3fe8aef
Alex Omarov <shtil.a@yandex.ru>
2022-08-29 13:21:45 +0700

    Add comments, new description files, add type hints

--- main/main.py
+++ main/main.py
@@ -1,4 +1,4 @@
 from tensorflow_start import start as start
 
 if __name__ == '__main__':
-    start.train()
+    start.train('accuracy')
--- main/tensorflow_start/start.py
+++ main/tensorflow_start/start.py
@@ -1,10 +1,39 @@
-# This is a Python script for tensorflow start.
-from tensorflow import keras
-from util import logger as logger
+"""
+Sequential neural network for classifying MNIST dataset
+
+This script allows the user to print to the console resulting tensor of sequential neural network for MNIST dataset.
+
+This tool accepts no parameters.
+
+This script requires that `tensorflow` be installed within the Python
+environment you are running this script in.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * train - prints the resulting tensor
+"""
+
+from tensorflow import keras as keras
+from util import logger as log
 
 
-def train():
-    logger.info(str.format("Keras version: {0}", keras.__version__))
+def train(metric: str):
+    """Prints resulting tensor of sequential NN for MNIST classification.
+
+    If the argument `metric` isn't passed in, the default accuracy metric is used.
+
+    Parameters
+    ----------
+    metric : str, optional
+             The metric for model compilation
+
+    Raises
+    ------
+    NotImplementedError
+        If passed metric isn't supported.
+    """
+    log.info(str.format("Keras version: {0}", keras.__version__))
 
     # Get basic vars
     (x_train, y_train), (x_test, y_test) = _get_dataset()  # x - images, y - labels
@@ -12,7 +41,7 @@ def train():
     loss_fn = _get_loss_function()
 
     # Train model
-    _compile_model(model, loss_fn)
+    _compile_model(model, loss_fn, metric)
     _fit(model, x_train, y_train)
     model.evaluate(x_test, y_test, verbose=2)
 
@@ -25,23 +54,23 @@ def train():
 
 # Private functions
 
-def _get_probability_model(model):
+def _get_probability_model(model: keras.Sequential) -> keras.Sequential:
     return keras.Sequential([model, keras.layers.Softmax()])
 
 
-def _fit(model, x_train, y_train):
+def _fit(model, x_train, y_train) -> None:
     model.fit(x_train, y_train, epochs=5)
 
 
-def _compile_model(model, loss_fn):
-    model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+def _compile_model(model, loss_fn, metric: str) -> None:
+    model.compile(optimizer='adam', loss=loss_fn, metrics=[metric])
 
 
-def _get_loss_function():
+def _get_loss_function() -> keras.losses.SparseCategoricalCrossentropy:
     return keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 
-def _get_model():
+def _get_model() -> keras.models.Sequential:
     # Create simple sequential model (each layer after another). Model - collection of layers
     model = keras.models.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),  # Flat incoming tensor to 28x28 matrix
@@ -49,11 +78,11 @@ def _get_model():
         keras.layers.Dropout(0.2),
         keras.layers.Dense(10)
     ])
-
+    log.info(str.format("Got model {0}", model))
     return model
 
 
-def _get_dataset():
+def _get_dataset() -> tuple:
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
     return (x_train, y_train), (x_test, y_test)
