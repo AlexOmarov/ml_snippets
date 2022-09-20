f45412f80725ca214f04529a622557f5ecfc4662
Alex Omarov <shtil.a@yandex.ru>
2022-09-20 22:01:53 +0700

    ML-3 Fix workflow
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

--- conftest.py
+++ conftest.py
@@ -4,6 +4,6 @@ import sys
 # Make sure that the application source directory (this directory's parent) is
 # on sys.path.
 
-there = os.path.dirname(os.path.dirname(os.path.abspath(__file__)) + "\\src\\main\\app\\")
+there = os.path.dirname(os.path.dirname(os.path.abspath(__file__)) + "/src/main/app/")
 sys.path.insert(0, there)
 print(sys.path)
