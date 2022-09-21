8cfa214e678344e3d8844ef5fd242a374768efcc
Alex Omarov <shtil.a@yandex.ru>
2022-09-21 22:29:55 +0700

    ML-5 Freq domain features
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

--- src/main/app/business/audio/analysis/analysis.py
+++ src/main/app/business/audio/analysis/analysis.py
@@ -5,6 +5,7 @@ import matplotlib.patches as mpatches
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy import ndarray
+from scipy import fft
 from werkzeug.datastructures import FileStorage
 
 #  App imports
@@ -16,17 +17,29 @@ _log = logger.get_logger(__name__.replace('__', '\''))
 
 
 def analyse(storage: FileStorage, frame_length: int, hop_length: int) -> AudioAnalysisResult:
-    return AudioAnalysisResult(_extract_features(storage, frame_length, hop_length), frame_length, hop_length)
-
-
-def _extract_features(storage: FileStorage, frame_length: int, hop_length: int) -> str:
     audio, sr = librosa.load(storage)
+    return AudioAnalysisResult(
+        time_features_plot_path=_extract_time_features(audio, sr, storage.filename, frame_length, hop_length),
+        freq_features_plot_path=_extract_freq_features(audio, sr, storage.filename),
+        frame_length=frame_length,
+        hop_length=hop_length
+    )
 
+
+def _extract_freq_features(audio, sr, filename: str) -> str:
+    ft = _get_fft(audio)
+    magnitude = np.absolute(ft)
+    frequency = np.linspace(0, sr, len(magnitude))
+
+    return _build_freq_plot(magnitude, frequency, filename)
+
+
+def _extract_time_features(audio, sr, filename: str, frame_length: int, hop_length: int) -> str:
     ae = _get_amplitude_envelope(audio, frame_length, hop_length)
     zcr = _get_zcr(audio, frame_length, hop_length)
     rms = _get_rms(audio, frame_length=frame_length, hop_length=hop_length)
 
-    return _build_plot(audio, ae, zcr, rms, storage.filename, sr, hop_length)
+    return _build_plot(audio, ae, zcr, rms, filename, sr, hop_length)
 
 
 def _get_amplitude_envelope(signal, frame_length: int, hop_length: int) -> ndarray:
@@ -41,12 +54,30 @@ def _get_zcr(audio, frame_length, hop_length):
     return librosa.feature.zero_crossing_rate(audio, frame_length=frame_length, hop_length=hop_length)[0]
 
 
+def _get_fft(audio):
+    return fft.fft(audio)
+
+
 def _get_rms(signal, frame_length, hop_length) -> ndarray:
     result = []
     for i in range(0, len(signal), hop_length):
         current_rms = np.sqrt(np.sum(signal[i:i + frame_length] ** 2) / frame_length)
         result.append(current_rms)
     return np.array(result)
+
+
+def _build_freq_plot(magnitude, frequency, filename: str) -> str:
+    path = Config.MODEL_PATH + filename + '_freq_features.png'
+    plt.figure(figsize=(15, 17))
+
+    plt.subplot(3, 1, 1)
+
+    plt.plot(frequency[:5000], magnitude[:5000])
+    plt.xlabel("Freq (Hz)")
+    plt.ylabel("Magnitude")
+    plt.title(filename)
+    plt.savefig(path)
+    return path
 
 
 def _build_plot(signal, ae, zcr, rms, filename: str, sr: int, hop_length: int) -> str:
--- src/main/app/presentation/api/audio_analysis_result.py
+++ src/main/app/presentation/api/audio_analysis_result.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class AudioAnalysisResult:
-    visuals_path: str
+    time_features_plot_path: str
+    freq_features_plot_path: str
     frame_length: int
     hop_length: int
 
@@ -11,5 +12,6 @@ class AudioAnalysisResult:
         return {
             'frame_length': self.frame_length,
             'hop_length': self.hop_length,
-            'visuals_path': self.visuals_path
+            'time_features_plot_path': self.time_features_plot_path,
+            'freq_features_plot_path': self.freq_features_plot_path
         }
