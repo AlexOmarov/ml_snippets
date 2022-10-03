4d785fa98022e979b2e8a346f0a743419ac4ffeb
Alex Omarov <shtil.a@yandex.ru>
2022-10-03 16:24:33 +0700

    ML-6 Refactoring
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

--- src/main/app/business/audio/analysis/analysis.py
+++ src/main/app/business/audio/analysis/analysis.py
@@ -1,14 +1,28 @@
-#  Lib imports
+"""
+Script for extracting features of audio file
+
+This script allows user to extract time/frequency domain features from audio file and save it as a plot.
+
+This tool accepts audio file, frame and hop length as input parameters.
+
+This script requires that `tensorflow` be installed within the Python environment.
+
+This file can also be imported as a module.
+
+Public interface:
+
+    * analyse - gets audio file, parses it as bytes and extracts features into plots.
+                Returns paths to created plots
+"""
+
 import librosa
 import librosa.display
-import matplotlib.patches as mpatches
+import matplotlib.patches as mpl_patches
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
 from numpy import ndarray
-from scipy import fft
 from werkzeug.datastructures import FileStorage
 
-#  App imports
 from business.util.ml_logger import logger
 from presentation.api.audio_analysis_result import AudioAnalysisResult
 from src.main.resource.config import Config
@@ -17,24 +31,35 @@ _log = logger.get_logger(__name__.replace('__', '\''))
 
 
 def analyse(storage: FileStorage, frame_length: int, hop_length: int) -> AudioAnalysisResult:
+    """
+    Performs analyse of the requested audio file
+
+    :param storage: Storage with audio file
+    :param frame_length: Length of a single frame
+    :param hop_length: Length of a single hop
+
+    :return AudioAnalysisResult
+
+    :exception NotImplementedError If passed metric isn't supported.
+
+    """
     audio, sr = librosa.load(storage)
-    return AudioAnalysisResult(
-        time_features_plot_path=_extract_time_features(audio, sr, storage.filename, frame_length, hop_length),
-        freq_features_plot_path=_extract_freq_features(audio, sr, storage.filename),
-        frame_length=frame_length,
-        hop_length=hop_length
-    )
+
+    time_feature_plot_path: str = _build_time_features_plot(audio, sr, storage.filename, frame_length, hop_length)
+    freq_feature_plot_path: str = _build_freq_features_plot(audio, sr, storage.filename, frame_length, hop_length)
+
+    return AudioAnalysisResult(time_feature_plot_path, freq_feature_plot_path, frame_length, hop_length)
 
 
-def _extract_freq_features(audio, sr, filename: str) -> str:
-    ft = _get_fft(audio)
+def _build_freq_features_plot(audio, sr, filename: str, frame_length: int, hop_length: int) -> str:
+    ft = _get_fft(audio, frame_length, hop_length)
     magnitude = np.absolute(ft)
     frequency = np.linspace(0, sr, len(magnitude))
-
+    _build_spectrogram_plot(magnitude, filename, sr, hop_length)
     return _build_freq_plot(magnitude, frequency, filename)
 
 
-def _extract_time_features(audio, sr, filename: str, frame_length: int, hop_length: int) -> str:
+def _build_time_features_plot(audio, sr, filename: str, frame_length: int, hop_length: int) -> str:
     ae = _get_amplitude_envelope(audio, frame_length, hop_length)
     zcr = _get_zcr(audio, frame_length, hop_length)
     rms = _get_rms(audio, frame_length=frame_length, hop_length=hop_length)
@@ -54,8 +79,8 @@ def _get_zcr(audio, frame_length, hop_length):
     return librosa.feature.zero_crossing_rate(audio, frame_length=frame_length, hop_length=hop_length)[0]
 
 
-def _get_fft(audio):
-    return fft.fft(audio)
+def _get_fft(audio, frame_length: int, hop_length: int):
+    return librosa.stft(audio, n_fft=frame_length, hop_length=hop_length)
 
 
 def _get_rms(signal, frame_length, hop_length) -> ndarray:
@@ -80,6 +105,22 @@ def _build_freq_plot(magnitude, frequency, filename: str) -> str:
     return path
 
 
+def _build_spectrogram_plot(magnitude, filename: str, sr, hop_length: int) -> str:
+    path = Config.MODEL_PATH + filename + '_spectrogram.png'
+    plt.figure(figsize=(15, 17))
+
+    plt.subplot(3, 1, 1)
+
+    librosa.display.specshow(
+        librosa.power_to_db(magnitude ** 2), sr=sr, hop_length=hop_length, x_axis="time", y_axis="log"
+    )
+    plt.colorbar(format="%+2.f")
+    plt.title(filename)
+    plt.savefig(path)
+
+    return path
+
+
 def _build_plot(signal, ae, zcr, rms, filename: str, sr: int, hop_length: int) -> str:
     path = Config.MODEL_PATH + filename + '_features.png'
     frames = range(0, ae.size)
@@ -88,10 +129,10 @@ def _build_plot(signal, ae, zcr, rms, filename: str, sr: int, hop_length: int) -
 
     plt.subplot(3, 1, 1)
 
-    red_patch = mpatches.Patch(color='red', label='Amplitude envelope')
-    blue_patch = mpatches.Patch(color='blue', label='Waveform')
-    green_patch = mpatches.Patch(color='green', label='RMS energy')
-    yellow_patch = mpatches.Patch(color='yellow', label='Zero crossing rate')
+    red_patch = mpl_patches.Patch(color='red', label='Amplitude envelope')
+    blue_patch = mpl_patches.Patch(color='blue', label='Waveform')
+    green_patch = mpl_patches.Patch(color='green', label='RMS energy')
+    yellow_patch = mpl_patches.Patch(color='yellow', label='Zero crossing rate')
     plt.legend(handles=[red_patch, blue_patch, green_patch, yellow_patch], fontsize=10, shadow=True, framealpha=0.5,
                edgecolor='b',
                title='Legend')
--- src/main/app/business/image/classification/mnist/mnist.py
+++ src/main/app/business/image/classification/mnist/mnist.py
@@ -5,16 +5,15 @@ This script allows user to classify images from mnist dataset or similar.
 
 This tool accepts no parameters.
 
-This script requires that `tensorflow` be installed within the Python
-environment you are running this script in.
+This script requires that `tensorflow` be installed within the Python environment.
 
-This file can also be imported as a module and contains the following functions:
+This file can also be imported as a module.
+Public interface:
 
     * train - trains model, set global model, transforms to tensorflow lite and saves on a drive
     * predict - gets an image and returns ClassificationResult with predicted class
 """
 
-#  Lib imports
 import numpy as np
 import tensorflow as tf
 from keras import Sequential
@@ -25,7 +24,6 @@ from tensorflow import keras as keras
 from tensorflow.python.framework.ops import EagerTensor
 from werkzeug.datastructures import FileStorage
 
-#  App imports
 from business.util.ml_logger import logger
 from business.util.ml_tensorboard import histogram_callback
 from presentation.api.classification_result import ClassificationResult
@@ -49,10 +47,7 @@ def predict(image: FileStorage) -> ClassificationResult:
     """
     Makes a prediction based on passed image.
 
-    Parameters
-    ----------
-    image : FileStorage
-             storage with image info
+    :param image: Storage with image info
     """
     result: str = _make_prediction(_preprocess_single_image(image.read()))
     return ClassificationResult(image_name=image.filename, label=result)
@@ -62,75 +57,50 @@ def train(metric: str) -> TrainResult:
     """
     Prepares model for predictions.
     Creates model, updates global model, writes model to disk and converts in to tensorflow lite
-
     If the argument `metric` isn't passed in, the default accuracy metric is used.
 
-    Parameters
-    ----------
-    metric : str, optional
-             The metric for model compilation
+    :param metric : The metric for model compilation (optional)
 
-    Raises
-    ------
-    NotImplementedError
-        If passed metric isn't supported.
+    :return TrainResult
+
+    :exception NotImplementedError If passed metric isn't supported.
     """
-    # Get basic vars
-    (x_train, y_train), (x_test, y_test) = _get_dataset()  # x - images, y - labels
+
+    (train_images, train_labels), (test_images, test_labels) = _get_dataset()
     model: Sequential = _get_model()
-    loss_fn: Loss = _get_loss_function()
+    loss_fn: Loss = SparseCategoricalCrossentropy(from_logits=True)
 
-    # Train model
-    _compile_model(model, loss_fn, metric)
-    _fit(model, x_train, y_train, [histogram_callback.get_histogram_callback(1)])
-    result: list = model.evaluate(x_test, y_test, verbose=2)
+    model.compile(optimizer="adam", loss=loss_fn, metrics=[metric])
+    model.fit(train_images, train_labels, epochs=5, callbacks=[histogram_callback.get_histogram_callback(1)])
+    result: list = model.evaluate(test_images, test_labels, verbose=2)
 
-    # Create probability model
     probability_model = _get_probability_model(model)
 
-    # Refresh model sources
     _refresh_model_sources(probability_model)
 
-    # Convert to Tensorflow lite
     _convert_to_lite(probability_model)
 
     return TrainResult(metric=metric, data=result)
 
 
-# Private functions
+def _make_prediction(tensor: EagerTensor) -> str:
+    result = _ERROR_LABEL
+    if '_global_model' in globals():
+        predictions = _global_model.predict(tensor)
+        result = np.argmax(predictions, axis=1)[0].__str__()
+    return result
+
 
 def _get_probability_model(model: Sequential) -> Sequential:
-    # Layer which has same amount of neurons as in previous layer and applies softmax alg for each neuron activation
-    # Sum of the neuron outputs = 1? each output in [0,1]
-    # TODO: not [0,1], why?
     model.add(keras.layers.Softmax())
     return model
 
 
-def _fit(model: keras.Sequential, x_train, y_train, callbacks: list[keras.callbacks.TensorBoard]) -> None:
-    # Train model with number of epochs
-    model.fit(x_train, y_train, epochs=5, callbacks=callbacks)
-
-
-def _compile_model(model: keras.Sequential, loss_fn, metric: str) -> None:
-    # Adding loss function, metric and optimizer to model
-    # Optimizer - algorithm which will be used for going through neurons and weights and changing weights
-    model.compile(optimizer="adam", loss=loss_fn, metrics=[metric])
-
-
-def _get_loss_function() -> Loss:
-    # Function which defines losses after each optimization loop (related to passed metric)
-    return SparseCategoricalCrossentropy(from_logits=True)
-
-
 def _get_model() -> Sequential:
-    # Create simple sequential model (each layer after another). Model - collection of layers
     model: Sequential = Sequential([
         Flatten(input_shape=_IMAGE_SIZE),  # Flat incoming 28x28 matrix to single vector
-        Dense(128, activation="relu"),  # Fully integrated within previous layer
-        # Delete neurons from previous layer with probability 0.2 (make it less over-trained, more sparse)
-        Dropout(0.2),
-        # Last output layer, which has 10 elements (one by each category)
+        Dense(128, activation="relu"),
+        Dropout(0.2),  # Delete neurons from previous layer with probability 0.2 (make it less over-trained)
         Dense(10)
     ])
     return model
@@ -144,33 +114,24 @@ def _get_dataset() -> tuple[tuple[ndarray, ndarray], tuple[ndarray, ndarray]]:
 
 
 def _convert_to_lite(model: keras.models.Sequential) -> None:
-    # Convert the model.
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     tflite_model = converter.convert()
-    # Save the model.
+
     with open(Config.MODEL_PATH + _MODEL_TFLITE_NAME, 'wb') as f:
         f.write(tflite_model)
 
 
 def _refresh_model_sources(model: keras.models.Sequential) -> None:
-    # Refresh model sources
     global _global_model
+
     keras.utils.plot_model(model, Config.MODEL_PATH + _MODEL_PLOT_NAME, show_shapes=True)
     model.save(Config.MODEL_PATH + _MODEL_NAME)
     _global_model = model
-
-
-def _make_prediction(tensor: EagerTensor) -> str:
-    result = _ERROR_LABEL
-    if '_global_model' in globals():
-        predictions = _global_model.predict(tensor)
-        result = np.argmax(predictions, axis=1)[0].__str__()
-    return result
 
 
 def _preprocess_single_image(image_bytes) -> EagerTensor:
     image = tf.image.decode_jpeg(image_bytes, channels=1)
     image = tf.image.resize(image, size=_IMAGE_SIZE)
     image: EagerTensor = tf.expand_dims(image[:, :, 0], 0)
-    image = image / _NORMALIZATION  # Normalize final tensor
+    image = image / _NORMALIZATION
     return image
