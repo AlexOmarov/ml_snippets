225626abc6c2a013e073c1500af020cb59dc15c9
Alex Omarov <33569929+AlexOmarov@users.noreply.github.com>
2022-10-05 15:21:12 +0700

    ML-8 (#1)
    Bug fixes and tests
    
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

--- src/main/app/business/util/ml_logger/logger.py
+++ src/main/app/business/util/ml_logger/logger.py
@@ -3,6 +3,14 @@ import logging
 _log_format = f"%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
 
 
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    # logger.addHandler(_get_file_handler())
+    logger.addHandler(_get_stream_handler())
+    return logger
+
+
 def _get_file_handler():
     file_handler = logging.FileHandler("x.log")
     file_handler.setLevel(logging.WARNING)
@@ -15,11 +23,3 @@ def _get_stream_handler():
     stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(logging.Formatter(_log_format))
     return stream_handler
-
-
-def get_logger(name):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    # logger.addHandler(_get_file_handler())
-    logger.addHandler(_get_stream_handler())
-    return logger
---
+++ src/test/app/business/util/logger_test.py
@@ -0,0 +1,33 @@
+import logging
+
+import business.util.ml_logger.logger as log_module
+
+
+class LoggerTests:
+    def setup(self):
+        print("basic setup into class")
+
+    def teardown(self):
+        print("basic teardown into class")
+
+    @classmethod
+    def setup_class(cls):
+        print("class setup")
+
+    @classmethod
+    def teardown_class(cls):
+        print("class teardown")
+
+    def setup_method(self, method):
+        print("method setup")
+
+    def teardown_method(self, method):
+        print("method teardown")
+
+    def test_created_logger(self):
+        logger_name = "TEST_LOGGER"
+        log = log_module.get_logger(logger_name)
+
+        assert (log is not None)
+        assert (log.name == logger_name)
+        assert (log.level == logging.INFO)
