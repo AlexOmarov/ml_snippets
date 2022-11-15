d235a5027c2a14c0e3cb4ed5509eae712de6e403
Alex Omarov <shtil.a@yandex.ru>
2022-11-15 17:29:24 +0700

    ML-21 Theory
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

--- src/main/app/business/theory/basic_tensorflow.py
+++ src/main/app/business/theory/basic_tensorflow.py
@@ -4,41 +4,82 @@
 
 import tensorflow as tf
 from flask import Request
+from numpy import ndarray
 
+from business.theory.model import CustomModel
 from presentation.api.theory_result import TheoryResult
 
 v1 = tf.compat.v1
 sess = v1.Session(config=v1.ConfigProto(log_device_placement=True, allow_soft_placement=True))
 
+_NORMALIZATION = 255.0
 
-def first(request: Request) -> TheoryResult:
+
+def first() -> TheoryResult:
     """
     :param
     """
-    with v1.variable_scope("shared_variables") as scope, tf.device('/gpu:3'):
-        v1.disable_eager_execution()
-        first_input = v1.placeholder(tf.float32, [1000, 784], name="first_input")
-        _my_network(first_input)
-        scope.reuse_variables()
-        second_input = v1.placeholder(tf.float32, [1000, 784], name="second_input")
-        _my_network(second_input)
+    # Parameters
+    global sess
+    learning_rate = 0.01
+    training_epochs = 1000
+    batch_size = 100
+    display_step = 1
+    with tf.Graph().as_default():
+        # mnist data image of shape 28*28=784
+        x = v1.placeholder("float", [None, 784])
+        # 0-9 digits recognition => 10 classes
+        y = v1.placeholder("float", [None, 10])
+        model = CustomModel(sess, "name")
+        output = model.inference(x)
+        cost = model.loss(output, y)
 
-    return TheoryResult("Request: " + request.__str__())
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        train_op = model.training(cost, global_step, learning_rate)
+        eval_op = model.evaluate(output, y)
+        init_op = v1.initialize_all_variables()
+        sess.run(init_op)
+        dataset = _get_dataset()
+        train = dataset[0]
+        test = dataset[1]
+
+        # Training cycle
+        for epoch in range(training_epochs):
+            total_batch = int(train[0].__sizeof__() / batch_size)
+            # Loop over all batches
+            for i in range(total_batch):
+                mbatch_x, mbatch_y = _get_batch(train, batch_size, i)
+                # Fit training using batch data
+                feed_dict = {x: mbatch_x, y: mbatch_y}
+                sess.run(train_op, feed_dict=feed_dict)
+                # Display logs per epoch step
+                if epoch % display_step == 0:
+                    val_feed_dict = {x: test[0], y: test[1]}
+                accuracy = sess.run(eval_op, feed_dict=val_feed_dict)
+                print("VALIDATION ERROR: {}".format(1 - accuracy))
+
+    print("Optimization Finished!")
+    test_feed_dict = {x: test[0], y: test[1]}
+    accuracy = sess.run(eval_op, feed_dict=test_feed_dict)
+    print("Test Accuracy:", accuracy)
+    return TheoryResult("Request: ")
 
 
-def _my_network(inp):
-    with v1.variable_scope("layer_1"):
-        output_1 = _layer(inp, [784, 100], [100])
-    with v1.variable_scope("layer_2"):
-        output_2 = _layer(output_1, [100, 50], [50])
-    with v1.variable_scope("layer_3"):
-        output_3 = _layer(output_2, [50, 10], [10])
-    return output_3
+def _get_dataset() -> tuple[tuple[ndarray, ndarray], tuple[ndarray, ndarray]]:
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    # Normalize image vectors (make values interval less)
+    x_train, x_test = x_train / _NORMALIZATION, x_test / _NORMALIZATION
+    return (x_train, y_train), (x_test, y_test)
 
 
-def _layer(inp, weight_shape, bias_shape):
-    weight_init = tf.random_uniform_initializer(minval=-1, maxval=1)
-    bias_init = tf.constant_initializer(value=0)
-    matrix_w = v1.get_variable("W", weight_shape, initializer=weight_init)
-    b = v1.get_variable("b", bias_shape, initializer=bias_init)
-    return tf.matmul(inp, matrix_w) + b
+def _get_batch(train: tuple[ndarray, ndarray], batch_size: int, batch_number: int) -> tuple[ndarray, ndarray]:
+    start = batch_number * batch_size
+    end = start + batch_size
+    if start < train[0].__sizeof__():
+        return train
+    if end > train[0].__sizeof__():
+        end = train[0].__sizeof__()
+    return train[0][start:end], train[0][start:end]
+
+# TODO: fix Session context managers are not re-entrant. Use `Session.as_default()` if you want to enter a session multiple times.
+first()
--- src/main/app/business/theory/model.py
+++ src/main/app/business/theory/model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.python.client.session import Session
 from tensorflow.python.platform.tf_logging import log
 from tensorflow.python.training.gradient_descent import GradientDescentOptimizer
 
@@ -6,33 +7,59 @@ v1 = tf.compat.v1
 
 
 class CustomModel:
-    def __init__(self):
+    session: Session
+    scope_name: str
+
+    def __init__(self, session, scope_name):
         print(self)
+        self.session = session
+        self.scope_name = scope_name
 
     def inference(self, x):
-        init = tf.constant_initializer(value=0)
-        matrix_w = v1.get_variable("W", [784, 10], initializer=init)
-        b = v1.get_variable("b", [10], initializer=init)
-        output = tf.nn.softmax(tf.matmul(x, matrix_w) + b)
-        return output
+        # here x has 784 columns and N rows where N = size of minibatch
+        with self.session:
+            with v1.variable_scope("hidden_1") as scope:
+                hidden_1 = self._layer(x, [784, 256], [256])
+                scope.reuse_variables()
+            with v1.variable_scope("hidden_2") as scope:
+                hidden_2 = self._layer(hidden_1, [256, 256], [256])
+                scope.reuse_variables()
+            with v1.variable_scope("output") as scope:
+                output = self._layer(hidden_2, [256, 10], [10])
+                scope.reuse_variables()
+            return output
+        # here x has 10 columns and N rows where N = size of minibatch
 
     def loss(self, output, y):
-        dot_product = y * log(output)
-        # Reduction along axis 0 collapses each column into a
-        # single value, whereas reduction along axis 1 collapses
-        # each row into a single value. In general, reduction along
-        # axis i collapses the ith dimension of a tensor to size 1.
-        xentropy = -tf.reduce_sum(dot_product, reduction_indices=1)
-        loss = tf.reduce_mean(xentropy)
-        return loss
+        with self.session:
+            # For each result in minibatch count the error
+            dot_product = y * log(output)
+            # Reduction along axis 0 collapses each column into a
+            # single value, whereas reduction along axis 1 collapses
+            # each row into a single value. In general, reduction along
+            # axis i collapses the ith dimension of a tensor to size 1.
+            xentropy = -tf.reduce_sum(dot_product, reduction_indices=1)
+            loss = tf.reduce_mean(xentropy)
+            return loss
 
-    def training(self, cost, global_step, learning_rate):
-        optimizer = GradientDescentOptimizer(learning_rate)
-        train_op = optimizer.minimize(cost,
-                                      global_step=global_step)
-        return train_op
+    def training(self, loss, global_step, learning_rate):
+        with self.session:
+            v1.scalar_mul("loss", loss)
+            optimizer = GradientDescentOptimizer(learning_rate)
+            train_op = optimizer.minimize(loss, global_step=global_step)
+            return train_op
 
     def evaluate(self, output, y):
-        correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        return accuracy
+        with self.session:
+            correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            return accuracy
+
+    def _layer(self, inp, weight_shape, bias_shape):
+        with self.session:
+            weight_stddev = (2.0 / weight_shape[0]) ** 0.5
+            w_init = tf.random_normal_initializer(stddev=weight_stddev)
+            bias_init = tf.constant_initializer(value=0)
+            W = v1.get_variable("W", weight_shape, initializer=w_init)
+            b = v1.get_variable("b", bias_shape, initializer=bias_init)
+            return tf.nn.relu(tf.matmul(inp, W) + b)
