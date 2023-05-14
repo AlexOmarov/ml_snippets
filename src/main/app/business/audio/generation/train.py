f52ac897985e58fbeb42397d32ba830e7aa28673
Alex Omarov <shtil.a@yandex.ru>
2023-05-14 22:48:34 +0700

    ML-22 Training network
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

--- src/main/app/business/audio/generation/train.py
+++ src/main/app/business/audio/generation/train.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 import tensorflow as tf
 from keras.layers import Input, Conv2D, Dense, Flatten, LSTM, concatenate, TimeDistributed, Masking
@@ -6,6 +7,7 @@ from keras.models import Model
 from keras.optimizers import Adam
 
 from business.audio.generation.dto.training_setting import TrainingSetting
+from business.audio.generation.dto.training_unit import TrainingUnit
 from business.audio.generation.training_setting import ts
 from business.util.ml_logger import logger
 
@@ -16,10 +18,12 @@ def train(setting: TrainingSetting):
     batches_amount = len(os.listdir(setting.paths_info.serialized_units_dir_path))
 
     model = _get_model(setting)
+    generator = _get_dataset_generator(setting)
     model.fit_generator(
-        generator=_get_dataset_generator(),
+        generator=generator,
         steps_per_epoch=batches_amount,
-        epochs=setting.hyper_params_info.num_epochs
+        epochs=setting.hyper_params_info.num_epochs,
+        shuffle=True
     )
 
 
@@ -27,7 +31,7 @@ def train(setting: TrainingSetting):
 
 def _get_model(setting: TrainingSetting) -> tf.keras.models.Model:
     # Input layer for MFCC db tensor
-    mfcc_input = Input(shape=(setting.hyper_params_info.batch_size, setting.num_mels, None), name='mfcc_input')
+    mfcc_input = Input(shape=(setting.num_mels, None, 1), name='mfcc_input')
     # Add a masking layer to handle variable-length sequences
     masked_mfcc = Masking()(mfcc_input)
 
@@ -36,12 +40,16 @@ def _get_model(setting: TrainingSetting) -> tf.keras.models.Model:
     conv2 = Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(conv1)
     flatten_mfcc = Flatten()(conv2)
 
+    # Reshape flatten_mfcc to match the number of timesteps
+    timesteps = None  # Set the appropriate number of timesteps
+    reshaped_mfcc = tf.keras.layers.Reshape((timesteps, -1))(flatten_mfcc)
+
     # Input layer for phonemes
-    phoneme_input = Input(shape=(setting.hyper_params_info.batch_size, None), name='phoneme_input')
+    phoneme_input = Input(shape=(None, 1), name='phoneme_input')
     masked_phoneme = Masking()(phoneme_input)
 
     # LSTM layer for MFCC processing with variable-length sequences
-    lstm_mfcc = LSTM(units=64, return_sequences=True)(flatten_mfcc)
+    lstm_mfcc = LSTM(units=64, return_sequences=True)(reshaped_mfcc)
     # Extract the last output of LSTM for fixed-length output
     mfcc_output = tf.keras.layers.Lambda(lambda x: x[:, :, -1, :])(lstm_mfcc)
 
@@ -68,8 +76,17 @@ def _get_model(setting: TrainingSetting) -> tf.keras.models.Model:
     return model
 
 
-def _get_dataset_generator(words_file: str, phonemes_file: str, audio_files_dir: str, tensor_length: int) -> tuple:
-    print()
+def _get_dataset_generator(setting: TrainingSetting):
+    batch_number = 1
+    while True:
+        filename = f"/serialized_batch_{batch_number}.pkl"
+        with open(setting.paths_info.serialized_units_dir_path + filename, 'rb') as file:
+            units: [TrainingUnit] = pickle.load(file)
+        batch_mfcc = [unit.mfcc_db for unit in units]
+        batch_phonemes = [unit.phonemes for unit in units]
+        batch_spectrogram = [unit.spectrogram for unit in units]
+        yield [batch_mfcc, batch_phonemes], batch_spectrogram
+        batch_number += 1
 
 
 train(ts)
